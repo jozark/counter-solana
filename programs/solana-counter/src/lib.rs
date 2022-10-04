a5701b3a4d3b6b963c0dd31b6a8b91f920242ccb
Jozark <jorekplus@gmail.com>
2022-10-04 09:02:42 +0200

    create counter and initialize program

--- programs/solana-counter/src/lib.rs
+++ programs/solana-counter/src/lib.rs
@@ -7,9 +7,33 @@ pub mod solana_counter {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let counter_account = &mut ctx.accounts.counter_account;
+        counter_account.counter = 0;
+        Ok(())
+    }
+
+    pub fn increase_counter(ctx: Context<IncreaseCounter>) -> Result<()> {
+        let counter_account = &mut ctx.accounts.counter_account;
+        counter_account.counter = counter_account.counter + 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 32)]
+    pub counter_account: Account<'info, CounterAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+#[derive(Accounts)]
+pub struct IncreaseCounter<'info> {
+    #[account(mut)]
+    pub counter_account: Account<'info, CounterAccount>
+}
+
+#[account]
+pub struct CounterAccount {
+    pub counter: u16
+}
